efc0d621822380711e1d621132ea2861d21e62c2
Walter M. Soto Reyes <wmsoto@hotmail.com>
2025-06-12 20:32:33 -0400

    Add MIT license

---
+++ Cbr.Net/ComicBookFormat.cs
@@ -0,0 +1,19 @@
+namespace Cbr.Net
+{
+    /// <summary>
+    /// Supported comic book container formats.
+    /// </summary>
+    public enum ComicBookFormat
+    {
+        /// <summary>RAR based container (.cbr).</summary>
+        Cbr,
+        /// <summary>ZIP based container (.cbz).</summary>
+        Cbz,
+        /// <summary>TAR based container (.cbt).</summary>
+        Cbt,
+        /// <summary>7-Zip based container (.cb7).</summary>
+        Cb7,
+        /// <summary>ACE based container (.cba).</summary>
+        Cba
+    }
+}
